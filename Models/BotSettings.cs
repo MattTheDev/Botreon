9ce56a7accdcab71d306aa5f624c68e138fbfaa7
Matt Smith <me@mattthedev.codes>
2020-11-02 20:48:14 -0600

    Finalized code. WIP - DO NOT RUN <3 Need to test.

--- Models/BotSettings.cs
+++ Models/BotSettings.cs
@@ -1,5 +1,5 @@
-﻿
-using System.Text.Json.Serialization;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Botreon.Models
 {
@@ -7,14 +7,38 @@ namespace Botreon.Models
     {
         public string Prefix { get; set; }
         public int PatreonCampaignId { get; set; }
+        public ulong DiscordGuildId { get; set; }
 
-        [JsonPropertyName("Tokens")]
+        [JsonProperty("Tokens")]
         public TokenSettings Tokens  { get; set; }
 
+        [JsonProperty("Timers")]
+        public TimerSettings Timers { get; set; }
+
+        [JsonProperty("TierRoles")]
+        public List<TierRole> TierRoles { get; set; }
+
         public class TokenSettings
-    {
+        {
             public string Discord { get; set; }
             public string Patreon { get; set; }
+        }
+
+        public class TimerSettings
+        {
+            public int RoleReconcilier { get; set; }
+        }
+
+        public class TierRole
+        {
+            [JsonProperty("TierName")]
+            public string TierName { get; set; }
+
+            [JsonProperty("TierId")]
+            public int TierId { get; set; }
+
+            [JsonProperty("RoleId")]
+            public ulong RoleId { get; set; }
         }
     }
 }
--- Models/Patreon/Patron.cs
+++ Models/Patreon/Patron.cs
@@ -1,9 +1,11 @@
-﻿namespace Botreon.Models.Patreon
+﻿using static Botreon.Models.BotSettings;
+
+namespace Botreon.Models.Patreon
 {
     public class Patron
     {
         public string PatreonId { get; set; }
         public string DiscordId { get; set; }
-        public int TierId { get; set; }
+        public TierRole TierRole { get; set; }
     }
 }
